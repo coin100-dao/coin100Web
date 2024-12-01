7be875aefa5ce9d99ddbf8175caf8a2e99ecee33
mehyar500 <mehyar500@gmail.com>
2024-12-01 22:30:43 +0000

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import { getTheme } from './theme/theme';
 import AppRoutes from './routes/Routes';
 import Layout from './Layout';
-import { useAppDispatch, useAppSelector } from './store/hooks';
-import { RootState, persistor } from './store/store';
-import { disconnectWallet } from './store/slices/web3Slice';
+import { persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const App: React.FC = () => {
@@ -19,30 +17,6 @@ const App: React.FC = () => {
   };
 
   const theme = React.useMemo(() => getTheme(mode), [mode]);
-
-  const dispatch = useAppDispatch();
-  const { connectedAt, walletAddress } = useAppSelector(
-    (state: RootState) => state.web3
-  );
-
-  React.useEffect(() => {
-    if (walletAddress && connectedAt) {
-      const currentTime = Date.now();
-      const elapsed = currentTime - connectedAt;
-      const fiveMinutes = 5 * 60 * 1000;
-      const timeLeft = fiveMinutes - elapsed;
-
-      if (timeLeft <= 0) {
-        dispatch(disconnectWallet());
-      } else {
-        const timer = setTimeout(() => {
-          dispatch(disconnectWallet());
-        }, timeLeft);
-
-        return () => clearTimeout(timer);
-      }
-    }
-  }, [walletAddress, connectedAt, dispatch]);
 
   return (
     <ThemeProvider theme={theme}>
